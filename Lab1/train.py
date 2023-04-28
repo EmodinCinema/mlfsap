70cc9da04a381f4c43200804fc9959689d3b5de2
Грабовский Александр Сергеевич <73514408+EmodinCinema@users.noreply.github.com>
2023-04-28 18:17:00 +0500

    version 2
    Лабораторная работа №2 - версия 2 (добавление тестовой визуализации и зависимости)

--- Lab1/train.py
+++ Lab1/train.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 if __name__ == "__main__":
     # Load a model
-    model = YOLO("yolov3-tinyu.yaml")  # build a new model from scratch
+    model = YOLO("yolov3-tiny.yaml")  # build a new model from scratch
     # model = YOLO("yolov8.pt")  # load a pretrained model (recommended for training)
 
     # Use the model
-    results = model.train(data="edu_train.yaml", epochs=30, batch=12, device=0, imgsz=640, patience=5,
+    results = model.train(data="edu_train.yaml", epochs=30, batch=12, imgsz=640, patience=5,
                           name='yolov3-tiny', v5loader=True)  # train the model
     results = model.val()  # evaluate model performance on the validation set
     # results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
